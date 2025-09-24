562dffb79d165d40d61b8aa27d628099de72ee50
d.nadunbothota <d.nadunbothota@gmail.com>
2025-09-24 14:29:12 +0000

    dependencies change

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
-// jest.config.js - uses @next/jest to respect next.config.js
-const nextJest = require('@next/jest');
-
-const createJestConfig = nextJest({
-  dir: './'
-});
-
 /** @type {import('jest').Config} */
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  testEnvironment: 'jsdom',
-  moduleDirectories: ['node_modules', '<rootDir>/'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  // If you have path aliases, map here. Example:
-  // moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
-};
-
-module.exports = createJestConfig(customJestConfig);
+module.exports = {
+    preset: 'ts-jest',                      // Use ts-jest for TypeScript support
+    testEnvironment: 'jsdom',               // Use jsdom for React component testing
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],  // Setup file for jest-dom
+    reporters: ['default', 'jest-junit'],   // Default + Jenkins JUnit reporter
+    collectCoverage: true,                  // Enable coverage collection
+    coverageDirectory: 'coverage',          // Output coverage reports here
+    moduleDirectories: ['node_modules', '<rootDir>/'], // Resolve modules from these paths
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+      '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',  // Transform TS/JSX using ts-jest
+    },
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/$1',          // Optional: handle path aliases
+    },
+  };
+  
