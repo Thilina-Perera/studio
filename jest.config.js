4b59bcf790524d9b3b7e61bcaf94cd823f94c281
NadunBothota <d.nadunbothota@gmail.com>
2025-09-28 20:24:31 +0530

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {
-      '^@/components/(.*)$': '<rootDir>/src/components/$1',
-      '^@/app/(.*)$': '<rootDir>/src/app/$1',
-    },
-  };
