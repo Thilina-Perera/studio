75a727702471c34f1d86d886bdd153d645417a8f
Thilina Perera <tp20021224@gmail.com>
2025-10-05 11:34:30 +0000

    use this image for the background insted https://sdmntpraustraliaeast.oa

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center p-4">
       <Image
-        src="https://sdmntpraustraliaeast.oaiusercontent.com/files/00000000-557c-61fa-a6a3-f20a893a51d2/raw?se=2025-10-05T12%3A21%3A21Z&sp=r&sv=2024-08-04&sr=b&scid=83113787-fa0b-51b6-9ecd-d23f01b28368&skoid=cb94e22a-e3df-4e6a-9e17-1696f40fa435&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-10-04T22%3A27%3A01Z&ske=2025-10-05T22%3A27%3A01Z&sks=b&skv=2024-08-04&sig=0is3xg55UBBBi7uoWeqM5jVgSg0hVOqRrpljivHJpUw%3D"
+        src="https://sdmntpraustraliaeast.oaiusercontent.com/files/00000000-b414-61fa-9586-5815355b1799/raw?se=2025-10-05T12%3A33%3A40Z&sp=r&sv=2024-08-04&sr=b&scid=2278696a-53e8-58e3-b170-c6dcb7bda862&skoid=cb94e22a-e3df-4e6a-9e17-1696f40fa435&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-10-04T21%3A41%3A59Z&ske=2025-10-05T21%3A41%3A59Z&sks=b&skv=2024-08-04&sig=zikrYsY9EGCknGR7BvaROvLHG5wAbfBzPdna9wMRvsk%3D"
         alt="Background"
         layout="fill"
         objectFit="cover"
